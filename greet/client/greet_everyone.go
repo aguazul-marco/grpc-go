6d81267ff7f0d86d325c9e5efb7e3ffebb957b45
Marco <105734479+aguazul-marco@users.noreply.github.com>
2023-02-28 09:41:59 -0600

    grpc first commit (#1)

---
+++ greet/client/greet_everyone.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/aguazul-marco/grpc-go/greet/proto"
+)
+
+func doGreetEveryone(c pb.GreetServiceClient) {
+	log.Println("doGreetEveryone was invoked")
+
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v\n", err)
+	}
+
+	requests := []*pb.GreetRequest{
+		{FirstName: "Marco"},
+		{FirstName: "Tori"},
+		{FirstName: "Sebastian"},
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for _, req := range requests {
+			log.Printf("Sending message: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("Error while receiving: %v\n", err)
+				break
+			}
+			log.Printf("Recieved: %v\n", res.Result)
+		}
+		close(waitc)
+	}()
+	<-waitc
+}
---
+++ greet/client/greet_many_times.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/aguazul-marco/grpc-go/greet/proto"
+)
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Println("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Marco",
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading the stream: %v\n", err)
+		}
+		log.Printf("GreetingManyTimes: %s\n", msg.Result)
+	}
+
+}
